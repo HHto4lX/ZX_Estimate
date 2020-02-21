513aa642ee0eff71f2950c09c477c3c9ffc503b1
alessandra cappati <alessandra.cappati@cern.ch>
2020-02-21 11:22:02 +0100

    plot FR script

--- plotter/plotFR.C
+++ plotter/plotFR.C
@@ -64,11 +64,12 @@ void plotFR(){
   fr_ele_EB->SetMarkerColor(kBlack);
   fr_ele_EB->SetLineStyle(1);
   fr_ele_EB->SetLineColor(kBlack);
-  fr_ele_EB->SetMaximum(0.2);
+  fr_ele_EB->SetMaximum(0.18);
   fr_ele_EB->Draw("APE1");
   TLegend* l_ele_EB = new TLegend(0.2,0.75,0.6,0.85);
   l_ele_EB->AddEntry(fr_ele_EB, "Electrons barrel", "lp");
   l_ele_EB->SetFillColor(kWhite);
+  l_ele_EB->SetLineColor(kWhite);
   l_ele_EB->Draw();
   if      (year==2016) c_ele_EB->SaveAs("FR_2016_electron_barrel.png");
   else if (year==2017) c_ele_EB->SaveAs("FR_2017_electron_barrel.png");
@@ -90,10 +91,55 @@ void plotFR(){
   TLegend* l_ele_EE = new TLegend(0.2,0.75,0.6,0.85);
   l_ele_EE->AddEntry(fr_ele_EE, "Electrons endcap", "lp");
   l_ele_EE->SetFillColor(kWhite);
+  l_ele_EE->SetLineColor(kWhite);
   l_ele_EE->Draw();
   if      (year==2016) c_ele_EE->SaveAs("FR_2016_electron_endcap.png");
   else if (year==2017) c_ele_EE->SaveAs("FR_2017_electron_endcap.png");
   else if (year==2018) c_ele_EE->SaveAs("FR_2018_electron_endcap.png");
+
+
+  TCanvas* c_muon_EB = new TCanvas("c_muon_EB","c_muon_EB",600,600);
+  c_muon_EB->cd();
+  fr_muon_EB->SetTitle("");
+  fr_muon_EB->GetXaxis()->SetTitle("pT (GeV)");
+  fr_muon_EB->GetYaxis()->SetTitle("fake rate");
+  fr_muon_EB->SetMarkerStyle(20);
+  fr_muon_EB->SetMarkerSize(0.5);
+  fr_muon_EB->SetMarkerColor(kBlack);
+  fr_muon_EB->SetLineStyle(1);
+  fr_muon_EB->SetLineColor(kBlack);
+  fr_muon_EB->SetMaximum(0.18);
+  fr_muon_EB->Draw("APE1");
+  TLegend* l_muon_EB = new TLegend(0.2,0.75,0.6,0.85);
+  l_muon_EB->AddEntry(fr_muon_EB, "Muons barrel", "lp");
+  l_muon_EB->SetFillColor(kWhite);
+  l_muon_EB->SetLineColor(kWhite);
+  l_muon_EB->Draw();
+  if      (year==2016) c_muon_EB->SaveAs("FR_2016_muon_barrel.png");
+  else if (year==2017) c_muon_EB->SaveAs("FR_2017_muon_barrel.png");
+  else if (year==2018) c_muon_EB->SaveAs("FR_2018_muon_barrel.png");
+
+
+  TCanvas* c_muon_EE = new TCanvas("c_muon_EE","c_muon_EE",600,600);
+  c_muon_EE->cd();
+  fr_muon_EE->SetTitle("");
+  fr_muon_EE->GetXaxis()->SetTitle("pT (GeV)");
+  fr_muon_EE->GetYaxis()->SetTitle("fake rate");
+  fr_muon_EE->SetMarkerStyle(20);
+  fr_muon_EE->SetMarkerSize(0.5);
+  fr_muon_EE->SetMarkerColor(kBlack);
+  fr_muon_EE->SetLineStyle(1);
+  fr_muon_EE->SetLineColor(kBlack);
+  fr_muon_EE->SetMaximum(0.2);
+  fr_muon_EE->Draw("APE1");
+  TLegend* l_muon_EE = new TLegend(0.2,0.75,0.6,0.85);
+  l_muon_EE->AddEntry(fr_muon_EE, "Muons endcap", "lp");
+  l_muon_EE->SetFillColor(kWhite);
+  l_muon_EE->SetLineColor(kWhite);
+  l_muon_EE->Draw();
+  if      (year==2016) c_muon_EE->SaveAs("FR_2016_muon_endcap.png");
+  else if (year==2017) c_muon_EE->SaveAs("FR_2017_muon_endcap.png");
+  else if (year==2018) c_muon_EE->SaveAs("FR_2018_muon_endcap.png");
 
 
 }
